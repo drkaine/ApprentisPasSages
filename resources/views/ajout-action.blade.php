966a9539af97840616fa0c3dccba7bba5555e424
darkaine <duretkevin@live.fr>
2021-03-30 10:03:22 +0200

    formulaire crud

---
+++ resources/views/ajout-action.blade.php
@@ -0,0 +1,22 @@
+@extends("barre-admin")
+
+@section("content")
+
+<div id="ban" class="container-fluid m-t-1 ban">
+    <h1 id="titreAssociation" style="box-sizing:border-box;">{{$nom}} </h1>
+</div>
+
+<form method="POST" action="accueil-admin">
+    <label for="nom">Nom:</label><br>
+    <input type="text" id="nom" name="nom" placeholder="" required  ><br>
+    <label for="nom">Module relié:</label><br>
+    <label for="nom">Catalogue relié:</label><br>
+    <button> Valider</button>
+    <button> Annuler</button>
+  </form>
+
+<section id="coupDeCoeur" class ="cdc">
+    <div class="m-t-1 ban2">
+    </div>
+</section>
+        @endsection
