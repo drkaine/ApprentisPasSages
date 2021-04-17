c0c49bdbe6d12dd56f4c8586e89794e6d6c93306
darkaine <duretkevin@live.fr>
2021-04-17 09:42:05 +0200

    gestionnaire user css finis

--- resources/views/admin/user-gestion.blade.php
+++ resources/views/admin/user-gestion.blade.php
@@ -11,12 +11,12 @@
     <div class="user">
         @foreach ($users as $user)
             <div class="user2">
-                {!! $user->email !!} {!! $user->nom !!}
+                Nom : {!! $user->name !!} <br> Mail: {!! $user->email !!} 
                 <div class="crud">        
                     <a href="{{route('EditController.editAdmin', ["eid"=>$user->id])}}" class="fas fa-edit"></a>
                     <a href="{{route('AdminController.demandeSuppression', ["choix"=>"user", "id1"=>$user->id])}}" class="fas fa-minus-circle"></a>
                 </div>
-            </div>
+            </div><br>
         @endforeach
     </div>    
     <div class="m-t-1 ban2"></div>
